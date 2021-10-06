8e888b385571b2c87de1e2d7098edc8fda8059ad
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-10-06 19:24:18 +0100

    Add an eventlistener to the submit button so that it posts data to the leaderboard api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './style.css';
+import { createUserAndScore, fetchFromApi } from './api';
+
+const submitButton = document.querySelector('.submit-btn');
+const refreshButton = document.querySelector('.refresh-button');
+const nameInput = document.getElementById('name');
+const scoreInput = document.getElementById('score');
+
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(nameInput.value);
+    console.log(scoreInput.value);
+  createUserAndScore(nameInput.value, scoreInput.value);
+console.log('Hello');
+})
+console.log(refreshButton);
+
